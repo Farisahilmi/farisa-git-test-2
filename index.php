1b6cc5cdb9b1f08c258bdbd75d4363bc839e77a5
Farisahilmi <farisahilmiexe@gmail.com>
2025-10-24 20:06:27 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro asperiores atque et? Nostrum magni dolor, explicabo mollitia cumque eos veritatis asperiores maxime soluta facere ex possimus, tempore non quibusdam odio.</p>
 </body>
 </html>
